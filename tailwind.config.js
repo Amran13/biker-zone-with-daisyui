85bed88de1e680e83575dfe1a02b6d3f0c319d92
amranhossain <amranhossainchy@gmail.com>
2023-07-26 21:13:21 +0600

    banner section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        color : {
+          'bike-btn' : '#E76F51',
+        }
+      },
     },
     plugins: [],
   }
